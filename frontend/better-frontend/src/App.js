821628703ac67bd4114b163e0e695f688861b003
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-05-17 15:12:21 +0200

    Add App.js api methods

--- frontend/better-frontend/src/App.js
+++ frontend/better-frontend/src/App.js
@@ -1,11 +1,35 @@
-
 import './App.css';
 import ToDoList from "./components/ToDoList";
 import {useEffect, useState} from "react";
-import {getTodosApi} from "./service/TodoApi";
+import {addTodoApi, deleteTodoApi, getTodosApi, updateTodoApi} from "./service/TodoApi";
 
-function App() {
+
+export default function App() {
+
   const [todos, setTodos] = useState([{description: "des", id: "123", status:"OPEN"}]);
+
+  const addTodo = (description) => {addTodoApi({description: description, status:"OPEN"}).then((todo) => setTodos(todos.push(todo)))};
+  const advanceTodo = (todo) => {
+    if (todo.status === "OPEN") {
+      todo.status = "IN_PROGRESS";
+    }
+    else if (todo.status === "IN_PROGRESS") {
+      todo.status = "DONE"
+    }
+    updateTodoApi(todo)
+        .then((newTodo) => {
+          setTodos(
+              todos.splice(
+                  todos.findIndex((todo) => todo.id === newTodo.id), 1, newTodo
+              )
+          )
+    })
+  };
+  const deleteTodo = (id) => {
+    deleteTodoApi(id)
+        .then(() => {
+          setTodos(todos.splice(todos.findIndex((todo) => todo.id === id), 0))
+        })};
 
   useEffect(() => {
     getTodos()
@@ -13,9 +37,6 @@ function App() {
 
   const getTodos = () => {getTodosApi().then((data ) => setTodos(data))};
 
-  const addTodo = (description) => {};
-  const advanceTodo = (id) => {};
-  const deleteTodo = (id) => {};
 
   return (
     <div className="App">
@@ -23,5 +44,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
