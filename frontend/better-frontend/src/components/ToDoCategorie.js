d507936912adffce1de8d3624f5fa11b77db193e
Florian Weber <florian.florianweber.weber@googlemail.com>
2021-05-18 10:59:54 +0200

    Add Layout

--- frontend/better-frontend/src/components/ToDoCategorie.js
+++ frontend/better-frontend/src/components/ToDoCategorie.js
@@ -1,9 +1,33 @@
 import ToDoCard from "./ToDoCard";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles(() => ({
+    TodoCategorie:{
+        display: "flex",
+        backgroundColor: 'hotpink',
+        padding: 0,
+    },
+
+    ToDoCategorieHeader: {
+        color: "blue",
+        borderColor: "black",
+        borderStyle: "solid",
+        borderRightWidth: 5,
+        borderLeftWidth: 0,
+        borderTopWidth: 0,
+        borderBottomWidth: 0,
+
+        margin: 0,
+    }
+}))
+
 
 function ToDoCategorie ({todos, categorieName, updateTodo, deleteTodo}) {
+    const classes = useStyles()
+
     return(
-        <div>
-            <p>{categorieName}</p>
+        <div className={classes.TodoCategorie}>
+            <h2 className={classes.ToDoCategorieHeader}>{categorieName}</h2>
             {todos.map((todo) => <ToDoCard updateTodo={updateTodo} deleteTodo={deleteTodo} toDo={todo} key={todo.id}/>)}
         </div>
     )
--- frontend/better-frontend/src/components/ToDoList.js
+++ frontend/better-frontend/src/components/ToDoList.js
@@ -1,11 +1,26 @@
 import ToDoCategorie from "./ToDoCategorie";
+import makeStyles from '@material-ui/core/styles/makeStyles'
+
+const useStyles = makeStyles(() => ({
+    TodoListContainer:{
+        display: "grid",
+        gridTemplateRows: "1fr 1fr 1fr",
+        gridRowGap: 15,
+
+        padding: 10,
+
+        backgroundColor: 'lightgrey',
+    }
+}))
 
 function ToDoList({todos, updateTodo, deleteTodo}) {
+    const classes = useStyles()
+
     return (
-        <div>
-            <ToDoCategorie key = {"open"} updateTodo={updateTodo} deleteTodo={deleteTodo} categorieName={"open"} todos={todos.filter((todo) => todo.status === "OPEN")}/>
-            <ToDoCategorie key = {"doing"} updateTodo={updateTodo} deleteTodo={deleteTodo} categorieName={"doing"} todos={todos.filter((todo) => todo.status === "IN_PROGRESS")}/>
-            <ToDoCategorie key = {"done"} updateTodo={updateTodo} deleteTodo={deleteTodo} categorieName={"done"} todos={todos.filter((todo) => todo.status === "DONE")}/>
+        <div className={classes.TodoListContainer}>
+            <ToDoCategorie key = {"open"} updateTodo={updateTodo} deleteTodo={deleteTodo} categorieName={"OPEN"} todos={todos.filter((todo) => todo.status === "OPEN")}/>
+            <ToDoCategorie key = {"doing"} updateTodo={updateTodo} deleteTodo={deleteTodo} categorieName={"DOING"} todos={todos.filter((todo) => todo.status === "IN_PROGRESS")}/>
+            <ToDoCategorie key = {"done"} updateTodo={updateTodo} deleteTodo={deleteTodo} categorieName={"DONE"} todos={todos.filter((todo) => todo.status === "DONE")}/>
         </div>)
 }
 
